0f45badbf5d80259a0d2d9fd97042e56a4311d97
charlyx78 <106395095+charlyx78@users.noreply.github.com>
2024-05-25 20:57:24 -0600

    correct compress images before uploading

--- backend/src/middlewares/uploadAttachment.js
+++ backend/src/middlewares/uploadAttachment.js
@@ -3,24 +3,9 @@ import fs from 'fs';
 import multer from 'multer';
 import sharp from 'sharp';
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        const uploadPath = `uploads/posts`;
-        
-        fs.mkdir(uploadPath, { recursive: true }, function(err) {
-            if (err) {
-                return cb(err);
-            }
-            cb(null, uploadPath);
-        });
-    },
-    filename: function(req, file, cb) {
-        let ext = path.extname(file.originalname);
-        cb(null, Date.now() + '-' + Math.random() + ext);
-    }
-});
+const storage = multer.memoryStorage(); // Almacenamiento en memoria
 
-const upload = multer({
+export const uploadFiles = multer({
     storage: storage,
     fileFilter: function(req, file, callback) {
         if (
@@ -38,42 +23,40 @@ const upload = multer({
     limits: {
         fileSize: 1024 * 1024 * 200
     }
-}).single('file'); // Asumiendo que el campo del formulario es 'file'
+});
 
-export const uploadFiles = (req, res, next) => {
-    upload(req, res, function(err) {
+export const handleUpload = async (req, res, next) => {
+    uploadFiles.single('file')(req, res, async function(err) {
         if (err) {
             return res.status(400).send({ message: 'Error uploading file', error: err.message });
         }
 
-        // Solo comprimir si el archivo es una imagen
-        if (req.file.mimetype.startsWith('image/')) {
-            const filePath = req.file.path;
-            const compressedFilePath = path.join(path.dirname(filePath), 'compressed-' + req.file.filename);
-            
-            sharp(filePath)
-                .resize(800) // Redimensionar la imagen a un ancho de 800px (puedes ajustar esto)
-                .toFormat('jpeg', { quality: 80 }) // Convertir a JPEG con calidad de 80%
-                .toFile(compressedFilePath, (err, info) => {
-                    if (err) {
-                        return res.status(500).send({ message: 'Error processing image', error: err.message });
-                    }
-                    
-                    // Opcionalmente, eliminar el archivo original sin comprimir
-                    fs.unlink(filePath, (err) => {
-                        if (err) {
-                            console.error('Error deleting original file:', err);
-                        }
-                    });
+        if (req.file) {
+            if (req.file.mimetype.startsWith('image/')) {
+                try {
+                    const uploadPath = 'uploads/posts';
+                    await fs.promises.mkdir(uploadPath, { recursive: true });
+
+                    const compressedFilePath = path.join(uploadPath, 'compressed-' + Date.now() + '-' + Math.random() + '.jpeg');
+
+                    await sharp(req.file.buffer)
+                        .resize(800) // Redimensionar la imagen a un ancho de 800px
+                        .toFormat('jpeg', { quality: 80 }) // Convertir a JPEG con calidad de 80%
+                        .toFile(compressedFilePath);
 
                     // Añadir el path del archivo comprimido a la request
                     req.file.path = compressedFilePath;
 
                     next();
-                });
+                } catch (error) {
+                    return res.status(500).send({ message: 'Error processing image', error: error.message });
+                }
+            } else {
+                // Si no es una imagen, simplemente pasar al siguiente middleware
+                next();
+            }
         } else {
-            // Si no es una imagen, simplemente pasar al siguiente middleware
-            next();
+            return res.status(400).send({ message: 'No file uploaded' });
         }
     });
 };
--- backend/src/middlewares/uploadProfilePicture.js
+++ backend/src/middlewares/uploadProfilePicture.js
@@ -3,22 +3,8 @@ import fs from 'fs';
 import multer from 'multer';
 import sharp from 'sharp';
 
-const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        const uploadPath = `uploads/${req.body.username}`;
-        
-        fs.mkdir(uploadPath, { recursive: true }, function(err) {
-            if (err) {
-                return cb(err);
-            }
-            cb(null, uploadPath);
-        });
-    },
-    filename: function(req, file, cb) {
-        let ext = path.extname(file.originalname)
-        cb(null, Date.now() + '-' + Math.random() + ext)
-    }
-});
+// Usar almacenamiento en memoria para multer
+const storage = multer.memoryStorage();
 
 const upload = multer({
     storage: storage,
@@ -26,7 +12,9 @@ const upload = multer({
         if (
             file.mimetype == "image/png" ||
             file.mimetype == "image/jpg" ||
-            file.mimetype == "image/jpeg"
+            file.mimetype == "image/jpeg" ||
+            file.mimetype == "video/mp4" ||
+            file.mimetype == "video/webm"
         ) {
             callback(null, true);
         } else {
@@ -34,39 +22,43 @@ const upload = multer({
         }
     },
     limits: {
-        fileSize: 1024 * 1024 * 20
+        fileSize: 1024 * 1024 * 200
     }
-}).single('image'); // Asumiendo que el campo del formulario es 'image'
+}).single('file'); // Asumiendo que el campo del formulario es 'file'
 
 export const uploadFiles = (req, res, next) => {
-    upload(req, res, function(err) {
+    upload(req, res, async function(err) {
         if (err) {
             return res.status(400).send({ message: 'Error uploading file', error: err.message });
         }
-        
-        // Comprimir la imagen usando sharp
-        const filePath = req.file.path;
-        const compressedFilePath = path.join(path.dirname(filePath), 'compressed-' + req.file.filename);
-        
-        sharp(filePath)
-            .resize(800) // Redimensionar la imagen a un ancho de 800px (puedes ajustar esto)
-            .toFormat('jpeg', { quality: 80 }) // Convertir a JPEG con calidad de 80%
-            .toFile(compressedFilePath, (err, info) => {
-                if (err) {
-                    return res.status(500).send({ message: 'Error processing image', error: err.message });
+
+        if (req.file) {
+            // Solo comprimir si el archivo es una imagen
+            if (req.file.mimetype.startsWith('image/')) {
+                try {
+                    const uploadPath = 'uploads/posts';
+                    await fs.promises.mkdir(uploadPath, { recursive: true });
+                    
+                    const compressedFilePath = path.join(uploadPath, 'compressed-' + Date.now() + '-' + Math.random() + '.jpeg');
+                    
+                    await sharp(req.file.buffer)
+                        .resize(800) // Redimensionar la imagen a un ancho de 800px (puedes ajustar esto)
+                        .toFormat('jpeg', { quality: 80 }) // Convertir a JPEG con calidad de 80%
+                        .toFile(compressedFilePath);
+                    
+                    // Añadir el path del archivo comprimido a la request
+                    req.file.path = compressedFilePath;
+
+                    next();
+                } catch (error) {
+                    return res.status(500).send({ message: 'Error processing image', error: error.message });
                 }
-                
-                // Opcionalmente, eliminar el archivo original sin comprimir
-                fs.unlink(filePath, (err) => {
-                    if (err) {
-                        console.error('Error deleting original file:', err);
-                    }
-                });
-                
-                // Añadir el path del archivo comprimido a la request
-                req.file.path = compressedFilePath;
-                
+            } else {
+                // Si no es una imagen, simplemente pasar al siguiente middleware
                 next();
-            });
+            }
+        } else {
+            return res.status(400).send({ message: 'No file uploaded' });
+        }
     });
 };
