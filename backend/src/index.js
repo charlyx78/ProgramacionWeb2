418e508848d76921cd5a11f9bc18380d4599be39
charlyx78 <106395095+charlyx78@users.noreply.github.com>
2024-05-25 19:12:19 -0600

    update cors origin

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,7 @@ connectDB();
 const server = createServer(app);
 export const io = new Server(server, {
     cors: {
-        origin: ['https://programacion-web2-self.vercel.app', 'https://www.pw2-diceapp.com'],
+        origin: ['https://programacion-web2-self.vercel.app', 'https://www.pw2-diceapp.com', 'http://http://localhost:5173'],
         credentials: true
     },
     pingTimeout: 60000
